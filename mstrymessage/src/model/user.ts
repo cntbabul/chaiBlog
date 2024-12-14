28bac6bea9daf626b1979c8d0de79c5337bae9e1
babul <babul@kali>
2024-12-14 09:49:25 +0530

    signup route

---
+++ mstrymessage/src/model/user.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const MessageScheme: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpire: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Please add a username"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    trim: true,
+    unique: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please add a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Please add a verifyCode"],
+  },
+  verifyCodeExpire: {
+    type: Date,
+    required: [true, "Please add a verifyCodeExpire"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageScheme],
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model("User", UserSchema);
+
+export default UserModel;
